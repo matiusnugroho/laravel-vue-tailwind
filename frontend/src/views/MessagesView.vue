a1cee040441d0ad67381242164de64d1bd0da581
adindaco <appswinda@gmail.com>
2024-03-06 10:02:20 +0700

    Edit Message Page

--- frontend/src/views/MessagesView.vue
+++ frontend/src/views/MessagesView.vue
@@ -2,6 +2,7 @@
   <div class="flex justify-center">
   <div class="w-full bg-white rounded-lg shadow-md p-4">
     <div class="flex flex-col items-center pb-4">
+      <h5 class="text-xl font-medium mt-4">This is Message page</h5>
       <img class="w-24 h-24 rounded-full mx-auto" src="https://gravatar.com/avatar/8e1a9afced6b5c2a9204004ba6974bac?s=400&d=robohash&r=x" alt="Profile picture">
       <h5 class="text-xl font-medium mt-4">John Doe</h5>
       <p class="text-gray-600 text-sm">Software Engineer</p>
