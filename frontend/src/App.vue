0436bde020e90c223a9f3ba632419223e9db0294
matiusnugroho <djmathuse@gmail.com>
2024-03-06 11:11:40 +0700

    Edit Homeview by matius

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,5 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from '@/components/HelloWorld.vue'
 import { ref } from 'vue';
 
 const isSidebarOpen = ref(true);
